3e08384b5f54fb76ab34c23bf5b943d99efaf118
Mat12 <56970402+Mat-12@users.noreply.github.com>
2020-12-03 16:52:24 +0100

    Primo commit

---
+++ ban.py
@@ -0,0 +1,86 @@
+import socket
+import threading
+from termcolor import colored
+
+stop_thread = False
+exit_volontario = False
+
+# Choosing Nickname
+nickname = input("Choose your nickname: ")
+
+if nickname.lower() == "admin":
+    password = input("Inserisci la password di amministratore: ")
+
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+# Listening to Server and Sending Nickname
+def receive():
+
+    global stop_thread, exit_volontario
+
+    while stop_thread == False:
+
+        try:
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
+
+                next_message = client.recv(1024).decode("utf-8")
+
+                if next_message == "PASS":
+                    client.send(password.encode("utf-8"))
+
+                    if client.recv(1024).decode("utf-8") == "NONACCETTO":
+                        print("Password errata!")
+                        client.send("EXIT".encode("utf-8"))
+                        stop_thread = True
+
+            elif message == "Conneso al server!":
+                print(colored((message), "yellow"))
+
+            elif "è atterrato!" in message or "è uscito!" in message:
+                print(colored(message, "yellow"))
+
+            elif message == "Un ADMIN è atterrato nella chat!":
+
+                print(colored(message, "yellow"))
+
+            else:
+                print(message)
+        except:
+            if exit_volontario == False:
+                print("Il server non è più Online! Esco")
+                stop_thread = True
+
+# Sending Messages To Server
+def write():
+    global stop_thread
+
+    while stop_thread == False:
+
+        message = input("")
+
+        if message == "exit":
+
+            print("Uscito!")
+
+            client.send("EXIT".encode("utf-8"))
+
+            exit_volontario = True
+            stop_thread = True
+        
+        message = f"{nickname}: {message}"
+
+        client.send(message.encode('utf-8'))
+
+
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
---
+++ server.py
@@ -0,0 +1,111 @@
+import socket
+import threading, json
+
+host = '127.0.0.1'
+port = 55555
+
+banned = {}
+
+users = {}
+
+def apri_file():
+
+    global banned
+
+    with open("banned.json", "r") as f:
+        banned = json.load(f)
+
+def salva_file():
+
+    global banned
+
+    with open("banned.json", "w") as f:
+        json.dump(banned, f)
+
+apri_file()
+
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in users.keys():
+        client.send(message)
+
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+
+            if message.decode("utf-8") == "EXIT":
+                    
+                del(users[client])
+                broadcast('{} è uscito!'.format(users[client]).encode('utf-8'))
+                print(users)
+                break
+
+            broadcast(message)
+        except:
+
+            broadcast('{} è uscito!'.format(users[client]).encode('utf-8'))
+            del(users[client])
+            break
+
+# Receiving / Listening Function
+def receive():
+
+    global banned
+
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('utf-8'))
+
+        nickname = client.recv(1024).decode('utf-8')
+
+        if nickname.lower() == "admin":
+            client.send("PASS".encode("utf-8"))
+
+            password = client.recv(1024).decode("utf-8")
+
+            if password != "securepass":
+                client.send("NONACCETTO".encode("utf-8"))
+                client.close()
+                continue
+                
+            else:
+                broadcast("Un ADMIN è atterrato nella chat!".encode("utf-8"))
+
+        elif nickname in banned:
+
+            client.send("ERROR-BAN".encode("utf-8"))
+            client.close()
+            continue
+            
+        elif str(nickname) in users.values():
+            client.send("ERROR-NICK".encode("utf-8"))
+            client.close()
+            continue
+
+        users[client] = str(nickname)
+
+        print(f"{nickname} è atterrato!")
+
+        if nickname != "admin":
+            broadcast(f"{nickname} è atterrato!".encode("utf-8"))
+
+        client.send("Connesso al server!".encode('utf-8'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("PRONTO!")
+receive()
